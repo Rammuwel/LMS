34967d1a4a8955a6637759362ca88666017300c3
ram muwel <shrirammuwel02017@gmail.com>
2025-04-10 00:10:53 +0530

    changing database url

--- server/config/conn.js
+++ server/config/conn.js
@@ -13,7 +13,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
  
     mongoose.connection.on('connected', ()=>console.log('Database connected'))
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
 
 export default connectDB;
